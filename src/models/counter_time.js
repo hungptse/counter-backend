fc46093e6601983056468d2b494c9743739caa4e
khanhBV <KhanhBVSE@gmail.com>
2019-09-28 18:55:00 +0700

    Counter API, CounterType API, CounterTime API, Invoice API

---
+++ src/models/counter_time.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var CounterTime = sequelize.define('CounterTime', {
+        value: { type: DataTypes.INTEGER, allowNull: false },
+        name: { type: DataTypes.STRING, allowNull: false },
+        is_deleted: { type: DataTypes.BOOLEAN, defaultValue: true },
+        counter_id: { type: DataTypes.INTEGER },
+    }, {
+        underscored: true,
+        tableName: 'counter_time',
+        modelName: 'counter_time',
+        sequelize: sequelize
+    });
+    return Counter;
+};
---
+++ src/models/invoice.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var Invoice = sequelize.define('Invoice', {
+        is_deleted: { type: DataTypes.BOOLEAN, defaultValue: true },
+        counter_id: { type: DataTypes.INTEGER},
+        type_id: { type: DataTypes.INTEGER },
+    }, {
+        underscored: true,
+        tableName: 'invoice',
+        modelName: 'invoice',
+        sequelize: sequelize
+    });
+    return Counter;
+};
